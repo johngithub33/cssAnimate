6154a61ec756e2bdac8c21ed3a54f3c5f63e8190
john <johngithub33@gmail.com>
2022-01-17 19:04:18 -0500

    .

--- app.js
+++ app.js
@@ -2,16 +2,18 @@
 
 var el = document.querySelector('.keyframeDiv')
 
-
 var btn = document.querySelector('button')
 btn.addEventListener('click', startAnimate);
 
 function startAnimate(){
-    
     el.classList.add('keyframeTrial');
 
     setTimeout( () => {el.style.animationPlayState = 'paused'}, 2500);
     setTimeout( () => {el.classList.remove('keyframeTrial')}, 5000)
-
-
 }
+
+
+buttonid.onclick = function() {
+    this.style.backgroundColor = 'green';
+    this.style.opacity = 0;
+  };
